b82d56842cbef6c95af3ad8bddac86a0f1f0d258
George <George@Desktop-George>
2026-05-09 14:08:42 -0400

    partial update to goomba movement

--- src/Goomba.java
+++ src/Goomba.java
@@ -6,9 +6,14 @@ public class Goomba extends Sprite
 	final static String[] pose = {"forward"};
 	
 
+	
+
 	public Goomba(int x, int y, int direction)
 	{
 		super("g", x, y, 40, 40, 4, 0, pose);
+		this.physics = true;
+		boolean moveDir = false;
+		
 	}
 	
 	public void draw(Graphics g)
@@ -16,4 +21,17 @@ public class Goomba extends Sprite
 		g.drawImage(animation[direction].nextImage(2), (int)(x-Camera.x), (int)(y-Camera.y), w, h, null);
 
 	}
+	
+	public void movement() {
+		if(moveDir == false)
+		{
+			vx = -10;
+		}
+		if(moveDir == true) {
+			vx == 10;
+		}
+
+	}
+	
+
 }
