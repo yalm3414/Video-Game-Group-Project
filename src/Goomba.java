84b05f138b7162048cfa04ea3870d7d80ee4e775
George <George@Desktop-George>
2026-05-08 21:21:12 -0400

    goomba constructor built

--- src/Goomba.java
+++ src/Goomba.java
@@ -1,5 +1,12 @@
 public class Goomba extends Sprite
 {
-  
+	final static String[] pose = {"goombaL", "goombaR"};
+	
+
+	public Goomba(int x, int y, int direction)
+	{
+		super("G", x, y, 45, 45, 2, 0, pose);
+	}
+	
 
 }
